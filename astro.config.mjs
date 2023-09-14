956f85ac8e46daa5c9e37b0c28b2bddda1ab0ea8
gneiru <noelrohi59@gmail.com>
2023-09-14 13:55:59 +0800

    try serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/static';
+import vercel from '@astrojs/vercel/serverless';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'server',
   adapter: vercel(),
   integrations: [tailwind({
       applyBaseStyles: false,
