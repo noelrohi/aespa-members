e9f94b4ecdbfc4fbbf98e4b5ce8fda9385c11696
gneiru <noelrohi59@gmail.com>
2023-09-14 13:47:58 +0800

    chore: vercel astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
+import vercel from '@astrojs/vercel/static';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel(),
   integrations: [tailwind({
       applyBaseStyles: false,
     }), react()]
